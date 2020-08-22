2f2c9e39f10ff53cec555927a1226b27ba9fa8b8
Syed Jafri <syedjafri1@yahoo.com>
2020-08-22 04:42:08 -0400

    Added variables and functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-$(document).ready(function() {
+
+$(document).ready(function(){
+
+    
+    var $input = $("#input");
+    var $submit = $("#submit");
     
 
+    $submit.on('click', function (event) {
+        event.preventDefault();
+        var inputVal = $input.val();
+        getGiphys(inputVal);
 
+    });
+
+// Make a get request to the giphy api with the input value
+
+function getGiphys() {
+    
+    
 
 });
 
