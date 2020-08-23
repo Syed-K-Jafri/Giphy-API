1a69c9c1ab6005934055a817fcd957428f97f3f1
Syed Jafri <syedjafri1@yahoo.com>
2020-08-23 00:20:40 -0400

    Added getJSON to call API

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
 
-$(document).ready(function () {
-    $("button").click(function(e) {
-        e.preventDefault();
-        var seeSaw = $('.search-box-input').val();
-        var resultElement =  $('#results')
 
+
+
+$(document).ready(function(){
+    $("#button").click(function(){
+        event.preventDefault();
+        let searchBox = $('#getter').val();
+
+        $.getJSON(`https://api.giphy.com/v1/gifs/search?q=${searchBox}&api_key=i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p&limit=20`, function(){
+            
+
+        }); //get json function
+       
         
-        $.ajax({
-            url: "//api.giphy.com/v1/gifs/search?q=" + seeSaw + "&api_key=2Zf3ESzIA3eYUPPmqdLntqJOO4gfPVhS",
-            method: 'GET',
-            data: {q:seesaw},
-            dataType: 'json',
-            success: function(response) {
-                console.log(response.data);
-            }
-        });
-    
-         
-     
-    
-    });
-
-        
-
-
+          
+       
+      
+       
+    }); // second function
 
     
-});
+
+}); // first function
 
 
+
+
+
+
+
+
+
+
+//https://api.giphy.com/v1/gifs/search?api_key=i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p&q=cats
+//var apikey = 'i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p';
+
+
+
+
+// $.getJSON(url, function(data) {
+    //console.log(data);
+   
+
+
+
+
+    // let api =  'https://api.giphy.com/v1/gifs/search?api_key=';
+//let apiKey = 'i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p';
+//let query = '&q=cats';
+//let url1 = api + apiKey + query;
+//let searchbox = $('#getter').val();
