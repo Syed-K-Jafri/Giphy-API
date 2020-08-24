f040841088a8a2235b515ba1ad7e6d089f9ef73c
Syed Jafri <syedjafri1@yahoo.com>
2020-08-23 21:23:56 -0400

    Edited for loop.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
             event.preventDefault();
             let searchBox = $('#getter').val();
-            apiUrl = `https://api.giphy.com/v1/gifs/search?q=${searchBox}&api_key=i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p&limit=10`
+            apiUrl = `https://api.giphy.com/v1/gifs/search?q=${searchBox}&api_key=i3eLWc366RmPZHe8Ff1rCrqWafjU1V7p&limit=30`
 
             $.ajax({
                 url: apiUrl,
@@ -19,22 +19,18 @@
                     }
                     
                     for(var i = 0; i < data.data.length; i++) {
+
                         var newImg = document.createElement("img");
                             newImg.src = data.data[i].images.original.url;
-                    document.body.appendChild(newImg);
-
+                            document.body.appendChild(newImg);
 
                     }   
 
-
                 }
-
-                
 
             });
 
         });
-
 
   });
 
